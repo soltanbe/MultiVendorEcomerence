c4a7527c34d633ab33fb2ed2d69da25d8f14c6bb
soltanb <soltanbe@gmail.com>
2025-05-30 07:31:41 +0300

    install new project laravel + react

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel + Inertia + React</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+</head>
+<body>
+@inertia
+</body>
+</html>
